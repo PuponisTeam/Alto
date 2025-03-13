f5be64efe43947ed9e44d65f5ec664eeaf267a4a
Alessio Garzia Marotta Brusco <alessiobrusco609@gmail.com>
2025-03-13 15:31:03 +0100

    Started creating DetailsView.

---
+++ Alto/Activities/Details/DetailContent.swift
@@ -0,0 +1,10 @@
+//
+//  DetailContent.swift
+//  Alto
+//
+//  Created by Alessio Garzia Marotta Brusco on 13/03/25.
+//
+
+protocol DetailContent {
+    
+}
--- Alto/Activities/Details/DetailsView.swift
+++ Alto/Activities/Details/DetailsView.swift
@@ -5,3 +5,26 @@
 //  Created by Alessio Garzia Marotta Brusco on 12/03/25.
 //
 
+import SwiftUI
+
+// Pressione, Modello 3D, Ossigeno, UV.
+
+extension DetailsView {
+    var details: [DetailItem] {
+        [
+            .init(title: "Pressure", measurement: Measurement(value: 3, unit: UnitPressure.hectopascals), foregroundColor: .blue, backgroundColor: .yellow),
+        ]
+    }
+}
+
+struct DetailsView: View {
+    static let spacing = 10.0
+    
+    var body: some View {
+        Text("Ciao")
+    }
+}
+
+#Preview {
+    DetailsView()
+}
